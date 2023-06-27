ac3e3b1cecb1845d396bc0fe5b231039575b33a0
Amanda Gilbert <amanda.gilbert2014@gmail.com>
2023-06-27 16:41:32 -0400

    Create env.sh

---
+++ configs/env.sh
@@ -0,0 +1,4 @@
+export TF_VAR_confluent_cloud_api_key="<CONFLUENT_CLOUD_API_KEY>"
+export TF_VAR_confluent_cloud_api_secret="<CONFLUENT_CLOUD_API_SECRET>" 
+export TF_VAR_confluent_cloud_env_id="<CONFLUENT_CLOUD_ENVIRONMENT_ID>"
+export TF_VAR_schema_registry_id="<SCHEMA REGISTRY ID>"
