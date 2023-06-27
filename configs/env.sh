5503846e936f0b57a1dd5678eccfe6fdb80a6ad7
Amanda Gilbert <amanda.gilbert2014@gmail.com>
2023-06-27 16:41:44 -0400

    Delete configs directory

--- configs/env.sh
+++
@@ -1,4 +0,0 @@
-export TF_VAR_confluent_cloud_api_key="<CONFLUENT_CLOUD_API_KEY>"
-export TF_VAR_confluent_cloud_api_secret="<CONFLUENT_CLOUD_API_SECRET>" 
-export TF_VAR_confluent_cloud_env_id="<CONFLUENT_CLOUD_ENVIRONMENT_ID>"
-export TF_VAR_schema_registry_id="<SCHEMA REGISTRY ID>"
